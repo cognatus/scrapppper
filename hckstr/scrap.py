be5e206440f0454dd093d1e8f90f210fb986e1d9
cognatus <diegojmartinezzm@gmail.com>
2019-03-31 16:27:19 -0600

    que corra

--- hckstr/scrap.py
+++ hckstr/scrap.py
@@ -4,6 +4,8 @@ import json
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(executable_path = '../chromedriver_linux64/chromedriver',chrome_options=options)
 driver.get('https://www.hackster.io/projects/')
 
