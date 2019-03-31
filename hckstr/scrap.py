2ac607b6bc218aa41515d3f6442e7398c585992f
cognatus <diegojmartinezzm@gmail.com>
2019-03-31 16:39:57 -0600

    intermediate

--- hckstr/scrap.py
+++ hckstr/scrap.py
@@ -11,8 +11,8 @@ driver.get('https://www.hackster.io/projects/')
 
 final = []
 
-for i in range(233, 347):
-    newPage = driver.get('https://www.hackster.io/projects/?difficulty=beginner&page='+str(i+1))
+for i in range(0, 112):
+    newPage = driver.get('https://www.hackster.io/projects/?difficulty=intermediate&page='+str(i+1))
     time.sleep(3)
     content = driver.find_element_by_css_selector('.hckui__grid__cell.hckui__layout__flex')
     links = driver.execute_script("return document.getElementsByClassName('project_card__imageContainer__1cw7g');")
@@ -23,5 +23,5 @@ for i in range(233, 347):
         aux['url'] = link.get_attribute('href')
         final.append(aux)
         print(aux['url'])
-    with open('beginner3.json', 'w') as f:  # writing JSON object
+    with open('intermediate1.json', 'w') as f:  # writing JSON object
         json.dump(final, f)
