a3513dcce0820ae0600ab87cbec13db866d2bc0d
cognatus <diegojmartinezzm@gmail.com>
2019-03-31 07:46:43 +0000

    madrugada new loco

--- gettodo.py
+++ gettodo.py
@@ -12,9 +12,9 @@ driver.get('https://www.instructables.com/')
 
 final = []
 
-with open('food.json') as json_file:  
+with open('outside.json') as json_file:  
     data = json.load(json_file)
-    for p in range(3040, len(data)):
+    for p in range(5688, len(data)):
         driver.get(data[p]['url'])
         print(str(p) + '.-' + data[p]['url'])
         url = data[p]['url']
@@ -73,6 +73,6 @@ with open('food.json') as json_file:
                 'steps': step_all
             })
             if len(final) % 20 == 0:
-                with open('food2_all.json', 'w') as f:  # writing JSON object
+                with open('outside3_all.json', 'w') as f:  # writing JSON object
                     json.dump(final, f)
 
