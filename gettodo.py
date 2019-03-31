f2076cb2ce30d31156ad7a01abff442483b1f7fd
cognatus <diegojmartinezzm@gmail.com>
2019-03-30 18:11:07 -0600

    test

--- gettodo.py
+++ gettodo.py
@@ -13,44 +13,43 @@ final = []
 with open('play.json') as json_file:  
     data = json.load(json_file)
     for p in range(0, len(data)):
-        if p > 468:
-            driver.get(data[p]['url'])
-            print(data[p]['url'])
+        driver.get(data[p]['url'])
+        print(data[p]['url'])
 
-            title = driver.find_element_by_css_selector('.header-title').text
-            category = driver.find_element_by_css_selector('.category').text
-            channel = driver.find_element_by_css_selector('.channel').text
-            esta =True
+        title = driver.find_element_by_css_selector('.header-title').text
+        category = driver.find_element_by_css_selector('.category').text
+        channel = driver.find_element_by_css_selector('.channel').text
+        esta =True
+        i = 1
+        step_all = []
+        
+        try:
+            while(esta):
+                step = driver.find_element_by_id('step'+str(i))
+                step_title = step.find_element_by_css_selector('.step-title').text
+                steps_imgs = step.find_element_by_css_selector('.gallery-link')
+                otro = driver.execute_script("return document.getElementById('step"+str(i)+"').getElementsByClassName('gallery-link')")
+                imgs = []
+
+                for img in otro:
+                    imgs.append(img.get_attribute('href'))
+                
+                step_all.append({
+                    'step_title': step_title,
+                    'step': i,
+                    'imgs': imgs
+                })
+                i = i +1
+        except NoSuchElementException as Exception:
             i = 1
-            step_all = []
-            
-            try:
-                while(esta):
-                    step = driver.find_element_by_id('step'+str(i))
-                    step_title = step.find_element_by_css_selector('.step-title').text
-                    steps_imgs = step.find_element_by_css_selector('.gallery-link')
-                    otro = driver.execute_script("return document.getElementById('step"+str(i)+"').getElementsByClassName('gallery-link')")
-                    imgs = []
+            esta = False
 
-                    for img in otro:
-                        imgs.append(img.get_attribute('href'))
-                    
-                    step_all.append({
-                        'step_title': step_title,
-                        'step': i,
-                        'imgs': imgs
-                    })
-                    i = i +1
-            except NoSuchElementException as Exception:
-                i = 1
-                esta = False
-
-            final.append({
-                'title': title,
-                'category': category,
-                'channel': channel,
-                'steps': step_all
-            })
-            if len(final) % 10 == 0:
-                with open('play2_all.json', 'w') as f:  # writing JSON object
-                    json.dump(final, f)
+        final.append({
+            'title': title,
+            'category': category,
+            'channel': channel,
+            'steps': step_all
+        })
+        if len(final) % 10 == 0:
+            with open('play_all.json', 'w') as f:  # writing JSON object
+                json.dump(final, f)
