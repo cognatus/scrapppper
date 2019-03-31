06f896756d5b075cbe6fdabf3af973d16910fcf9
cognatus <diegojmartinezzm@gmail.com>
2019-03-31 16:22:43 -0600

    weee

---
+++ hckstr/scrap.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+import json
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(executable_path = '../chromedriver_linux64/chromedriver',chrome_options=options)
+driver.get('https://www.hackster.io/projects/')
+
+final = []
+
+for i in range(0, 116):
+    newPage = driver.get('https://www.hackster.io/projects/?difficulty=beginner&page='+str(i+1))
+    time.sleep(3)
+    content = driver.find_element_by_css_selector('.hckui__grid__cell.hckui__layout__flex')
+    links = driver.execute_script("return document.getElementsByClassName('project_card__imageContainer__1cw7g');")
+
+    print(i)
+    for link in links:
+        aux = {}
+        aux['url'] = link.get_attribute('href')
+        final.append(aux)
+        print(aux['url'])
+    with open('beginner1.json', 'w') as f:  # writing JSON object
+        json.dump(final, f)
+    break
