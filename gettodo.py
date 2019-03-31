83c3e9629ec5ead55f8b3ba7e6e747841a6e7290
cognatus <diegojmartinezzm@gmail.com>
2019-03-31 08:07:42 -0600

    workshop new new

--- gettodo.py
+++ gettodo.py
@@ -12,9 +12,9 @@ driver.get('https://www.instructables.com/')
 
 final = []
 
-with open('technology.json') as json_file:  
+with open('workshop.json') as json_file:  
     data = json.load(json_file)
-    for p in range(8400, len(data)):
+    for p in range(4940, len(data)):
         driver.get(data[p]['url'])
         print(str(p) + '.-' + data[p]['url'])
         url = data[p]['url']
@@ -24,12 +24,15 @@ with open('technology.json') as json_file:
         pasa = True
         try:
             title = driver.find_element_by_css_selector('.header-title').text
-            category = driver.find_element_by_css_selector('.category').text
-            channel = driver.find_element_by_css_selector('.channel').text
         except NoSuchElementException as Exception:
             pasa = False        
             print('no info')
         if pasa:
+            try:
+                category = driver.find_element_by_css_selector('.category').text
+                channel = driver.find_element_by_css_selector('.channel').text
+            except NoSuchElementException as Exception:  
+                print('no info headers')
             
             #intro = driver.find_element_by_id('intro')
             description = ''
@@ -73,6 +76,6 @@ with open('technology.json') as json_file:
                 'steps': step_all
             })
             if len(final) % 20 == 0:
-                with open('technology3_all.json', 'w') as f:  # writing JSON object
+                with open('workshop3_all.json', 'w') as f:  # writing JSON object
                     json.dump(final, f)
 
