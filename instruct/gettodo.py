85dc3649fc7451e3f494971a9c705ae4877d0164
cognatus <diegojmartinezzm@gmail.com>
2019-04-06 02:04:42 -0600

    play scrap

--- instruct/gettodo.py
+++ instruct/gettodo.py
@@ -16,7 +16,7 @@ wait = WebDriverWait(driver, 5)
 
 final = []
 errors = []
-file_name = 'outside'
+file_name = 'play'
 
 with open('all_'+file_name+'_links.json') as json_file:  
     data = json.load(json_file)
