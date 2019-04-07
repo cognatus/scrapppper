2b6c112d1dd67b02026501b1e99094032e7d12d1
cognatus <diegojmartinezzm@gmail.com>
2019-04-07 15:35:08 -0500

    home

--- instruct/beauty.py
+++ instruct/beauty.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 final = []
 errors = []
-file_name = 'food'
+file_name = 'home'
 
 with open('all_'+file_name+'_links.json') as json_file:  
     data = json.load(json_file)
