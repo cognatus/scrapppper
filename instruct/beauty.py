f4efdd1eedfdf9f5475065ee2c98c6486f8ed28c
cognatus <diegojmartinezzm@gmail.com>
2019-04-07 21:55:23 -0500

    technology5

--- instruct/beauty.py
+++ instruct/beauty.py
@@ -8,7 +8,7 @@ file_name = 'technology'
 
 with open('all_'+file_name+'_links.json') as json_file:  
     data = json.load(json_file)
-    for p in range(74473, 83780):
+    for p in range(46547, 55853):
         try:
             url = data[p]['url']
             response = get(url)
@@ -70,7 +70,7 @@ with open('all_'+file_name+'_links.json') as json_file:
                 })
 
                 if len(final) % 10 == 0:
-                    with open(file_name+'4_scrap.json', 'w') as f:  # writing JSON object
+                    with open(file_name+'5_scrap.json', 'w') as f:  # writing JSON object
                         json.dump(final, f)
             else:
                 print('nel')
@@ -78,5 +78,5 @@ with open('all_'+file_name+'_links.json') as json_file:
             print(str(p) + '.-' + data[p]['url'])
             print(e)
             errors.append(data[p]['url'])
-            with open(file_name+'4_error.json', 'w') as f:  # writing JSON object
+            with open(file_name+'5_error.json', 'w') as f:  # writing JSON object
                 json.dump(errors, f)
