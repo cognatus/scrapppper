eb5dceaf1cdea6c72663dfee2a9928d09661a807
cognatus <diegojmartinezzm@gmail.com>
2019-04-08 16:13:55 -0500

    workshop2

---
+++ instruct/beauty2.py
@@ -0,0 +1,38 @@
+from requests import get
+import json
+from bs4 import BeautifulSoup
+
+final = []
+errors = []
+file_name = 'workshop'
+
+with open(file_name+'_scrap.json') as json_file:  
+    data = json.load(json_file)
+    for p in range(0, len(data)):
+        try:
+            article = data[p]
+            url = article['url']
+            response = get(url)
+            html_soup = BeautifulSoup(response.text, 'html.parser')
+            print(str(p) + '.-' + url)
+
+            article['category'] = file_name
+            channel = ''
+            try:
+                channel = html_soup.find(class_ = 'channel').text
+            except Exception as e:  
+                channel = 'No channel to show'
+
+            article['channel'] = channel
+
+            final.append(article)
+
+            if len(final) % 10 == 0:
+                with open(file_name+'_scrap2.json', 'w') as f:  # writing JSON object
+                    json.dump(final, f)
+        except Exception as e:
+            print(str(p) + '.-' + data[p]['url'])
+            print(e)
+            errors.append(data[p]['url'])
+            with open(file_name+'_error.json', 'w') as f:  # writing JSON object
+                json.dump(errors, f)
